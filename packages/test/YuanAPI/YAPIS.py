e2a127352c71f0c35bd31b8d4329fb795dcfbe22
xiaoyy20d <xiaoyy20d@gmail.com>
2025-05-31 17:38:50 +0800

    Remove ignored files from repository

--- packages/test/YuanAPI/YAPIS.py
+++
@@ -1,9 +0,0 @@
-from ._apis import Globals, Default
-
-
-class YAPIEngine:
-    def __init__(self, raw):
-        self.string: Default.string = raw.string
-        self.onClicked: Default.onClicked = raw.clicked
-        # ---
-        self.globals: Globals = raw.API_G
